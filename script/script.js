4e9caf59b527f3f16dbadb6a2bf196bab05693b0
Cody <simoncody121@gmail.com>
2023-10-13 17:52:27 -0500

    setting up JS func

--- script/script.js
+++ script/script.js
@@ -3,5 +3,10 @@
 // return value in date time format to display
 
 calculateEta = () => {
-    document.getElementById('disP').innerHTML
+    let currentTime = new Date();
+    const mile = document.getElementById('milage').value;
+ 
+    console.log(currentTime + mile / 55);   
 }
+
+let ut = document.getElementById("updatedTime")
