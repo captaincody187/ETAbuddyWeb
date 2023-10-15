49b1dcdf8b0bb6437b912c5a94f2cfc1ae6b8c4d
Cody <simoncody121@gmail.com>
2023-10-14 19:51:15 -0500

    complete functionality and start on about page

--- script/script.js
+++ script/script.js
@@ -1,12 +1,38 @@
-// Get milage from input box
-// divide milage by 55
-// return value in date time format to display
 
 calculateEta = () => {
-    let currentTime = new Date();
-    const mile = document.getElementById('milage').value;
- 
-    console.log(currentTime + mile / 55);   
-}
+    // Get input from the user and divide by 55 mph
+    const milage = parseFloat(document.getElementById('milage').value);
+    const eta = milage / 55;
+  
+    // Convert ETA to milliseconds (1 hour = 3600000 milliseconds)
+    const intEta = Math.floor(eta * 3600000);
+  
+    // Create a new Date object
+    const now = new Date();
+  
+    // Calculate the ETA time in milliseconds
+    const etaTime = now.getTime() + intEta;
+  
+    // Create a new Date object with the ETA time
+    const etaDate = new Date(etaTime);
+  
+    // Set the time zone to Central Standard Time (CST)
+    const options = { timeZone: 'America/Chicago' };
+    
+    // Format the ETA time to a string in CST
+    const etaString = etaDate.toLocaleString('en-US',
+    // Format for mitlitary time 
+    { hour12: false },
+    options);
+  
+    document.getElementById("dateTime").innerHTML = etaString;
+    //console.log('Estimated Time of Arrival in CST:', etaString);
+  }
 
-let ut = document.getElementById("updatedTime")
+  // Clear the display area
+
+  clearEta = () => {
+    return document.getElementById('dateTime',).innerHTML = "";
+  }
+  
+
