71d7c70e08f56bc922a7f2d1ebb5c0b8f64c0aa4
cody simon <86523258+captaincody187@users.noreply.github.com>
2023-11-23 19:12:58 -0600

    storage and ten hour calc

--- script/script.js
+++ script/script.js
@@ -1,45 +1,144 @@
+// Captain Cody was here
+
+let currentETA = "";
+let tenHoursAdded = false;
+let etaStringG;
 
 calculateEta = () => {
+  tenHoursAdded = false;
 
-    // Get input from the user and divide by 55 mph
-    const milage = parseFloat(document.getElementById('milage').value);
-    const eta = milage / 55;
+  const milage = parseFloat(
+    document.getElementById("floatingInputValue").value
+  );
+  const eta = milage / 55;
+  const intEta = Math.floor(eta * 3600000);
+  const now = new Date();
+  const etaTime = now.getTime() + intEta;
+  const etaDate = new Date(etaTime);
+  const options = { timeZone: "America/Chicago" };
+  const etaString = etaDate.toLocaleString("en-US", { hour12: false }, options);
+  etaStringG = etaString;
 
-    // Convert ETA to milliseconds (1 hour = 3600000 milliseconds)
-    const intEta = Math.floor(eta * 3600000);
-  
-    // Create a new Date object
-    const now = new Date();
-  
-    // Calculate the ETA time in milliseconds
-    const etaTime = now.getTime() + intEta;
-  
-    // Create a new Date object with the ETA time
-    const etaDate = new Date(etaTime);
-  
-    // Set the time zone to Central Standard Time (CST)
-    const options = { timeZone: 'America/Chicago' };
-    
-    // Format the ETA time to a string in CST
-    const etaString = etaDate.toLocaleString('en-US',
-    // Format for mitlitary time 
-    { hour12: false },
-    options);
-  
-    document.getElementById("dateTime").innerHTML = etaString + " CST";
-    console.log('captainCody was here');
+  document.getElementById("dateTime").innerHTML = etaString + " CST";
 
-    // Alert to handle input value of 0
-    if(milage == 0){
-        alert('Please enter a valid number')
-    } 
-    
+  const slider = document.getElementById("flexSwitchCheckDefault");
+  if (slider.checked) {
+    slider.checked = false;
   }
 
-  // Clear the display area
+  tenHoursAdded = false;
 
-  clearEta = () => {
-    return document.getElementById('dateTime',).innerHTML = "";
+  const checkbox = document.getElementById("flexSwitchCheckDefault");
+  if (checkbox && checkbox.checked) {
+    checkbox.checked = false;
   }
-  
 
+  // Store ETA in sessionStorage
+  sessionStorage.setItem("storedETA", JSON.stringify(etaStringG));
+};
+
+window.onload = function () {
+  const storedData = sessionStorage.getItem("storedETA");
+  etaStringG = storedData ? JSON.parse(storedData) : null;
+
+  if (etaStringG) {
+    document.getElementById("dateTime").innerHTML = etaStringG + " CST";
+  }
+};
+
+const checkbox = document.getElementById("flexSwitchCheckDefault");
+
+tenBreak = () => {
+  const checkbox = document.getElementById("flexSwitchCheckDefault");
+
+  if (checkbox.checked && !tenHoursAdded) {
+    const etaDate = new Date(etaStringG);
+    etaDate.setTime(etaDate.getTime() + 36000000);
+    const updatedEtaString = etaDate.toLocaleString("en-US", { hour12: false });
+    document.getElementById("dateTime").innerHTML = updatedEtaString + " CST";
+    etaStringG = updatedEtaString;
+    tenHoursAdded = true;
+    console.log("Updated ETA with 10 hours:", updatedEtaString);
+  } else if (!checkbox.checked) {
+    tenHoursAdded = false;
+  }
+};
+
+checkbox.addEventListener("change", tenBreak);
+
+storeFunc = () => {
+  const etaToStore = currentETA || etaStringG;
+
+  const firstRow = document.getElementById("etaOne");
+  const secondRow = document.getElementById("etaTwo");
+  const thirdRow = document.getElementById("etaThree");
+
+  if (firstRow.innerHTML === "") {
+    firstRow.innerHTML = etaToStore;
+  } else if (secondRow.innerHTML === "") {
+    secondRow.innerHTML = etaToStore;
+  } else if (thirdRow.innerHTML === "") {
+    thirdRow.innerHTML = etaToStore;
+  } else {
+    alert("Please delete an ETA to store another.");
+  }
+};
+
+clearEta = () => {
+  currentETA = "";
+  tenHoursAdded = false;
+  etaStringG = "";
+
+  document.getElementById("dateTime").innerHTML = "";
+  document.getElementById("floatingInputValue").value = null;
+
+  const slider = document.getElementById("flexSwitchCheckDefault");
+  if (slider.checked) {
+    slider.checked = false;
+  }
+
+  // Clear sessionStorage for stored ETA
+  sessionStorage.removeItem("storedETA");
+};
+
+clearOne = () => {
+  return (document.getElementById("etaOne").innerHTML = "");
+};
+
+clearTwo = () => {
+  return (document.getElementById("etaTwo").innerHTML = "");
+};
+
+clearThree = () => {
+  return (document.getElementById("etaThree").innerHTML = "");
+};
+
+function resetSlider() {
+  const checkbox = document.getElementById("flexSwitchCheckDefault");
+  if (checkbox && checkbox.checked) {
+    checkbox.checked = false;
+  }
+}
+
+document
+  .getElementById("floatingInputValue")
+  .addEventListener("click", resetSlider);
+
+toggleText = () => {
+  const switchLabel = document.getElementById("switchLabel");
+  const checkbox = document.getElementById("flexSwitchCheckDefault");
+
+  if (checkbox.checked) {
+    switchLabel.classList.remove("text-muted");
+    alert("Ten (10) hours will be added to the ETA");
+  } else {
+    switchLabel.classList.add("text-muted");
+  }
+};
+
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
